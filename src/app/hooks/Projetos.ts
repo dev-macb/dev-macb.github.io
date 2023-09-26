53c3561330fb5e76826f6cfff31328226fda5952
Miguel Alves Cordeiro Braz <dev.macb@gmail.com>
2023-09-26 19:18:46 -0300

    Adição do projeto Breve Link
    Adição de hook para o novo projeto.

--- src/app/hooks/Projetos.ts
+++ src/app/hooks/Projetos.ts
@@ -1,6 +1,12 @@
 // Textos do carrossel
 const projetos = [
     {
+        titulo: 'MacB Theme',
+        categoria: 'Tema',
+        texto: 'O MacB é um tema minimalista que não só oferece uma estética limpa e moderna, mas também proporciona facilidade de personalização e realce de sintaxe avançado. Projetado para elevar a produtividade e a qualidade do código, o MacB é a escolha ideal para desenvolvedores que buscam uma experiência de desenvolvimento superior no Visual Studio Code.',
+        link: 'https://marketplace.visualstudio.com/items?itemName=dev-macb.macb-vscode-theme'
+    },
+    {
         titulo: 'Ambivar',
         categoria: 'Pacote',
         texto: 'O Ambivar é um pacote em PHP que tem como objetivo facilitar a gestão de variáveis de ambiente em projetos. Esse pacote permite carregar essas variáveis através de um arquivo .env, que é lido automaticamente ou especificando o diretório do arquivo.',
@@ -19,10 +25,10 @@ const projetos = [
         link: 'https://packagist.org/packages/dev-macb/poderoso'
     },
     {
-        titulo: 'MacB Theme',
-        categoria: 'Tema',
-        texto: 'O MacB é um tema minimalista que não só oferece uma estética limpa e moderna, mas também proporciona facilidade de personalização e realce de sintaxe avançado. Projetado para elevar a produtividade e a qualidade do código, o MacB é a escolha ideal para desenvolvedores que buscam uma experiência de desenvolvimento superior no Visual Studio Code.',
-        link: 'https://marketplace.visualstudio.com/items?itemName=dev-macb.macb-vscode-theme'
+        titulo: 'Breve Link',
+        categoria: 'API',
+        texto: 'O Breve Link é uma prática API de encurtamento de URLs, tornando links longos mais curtos e fáceis de compartilhar. É uma solução simples e eficaz para simplificar o compartilhamento na web.',
+        link: 'https://github.com/dev-macb/breve-link'
     }
 ];
 
