4894e208228e9bd51c19210b380edcfe66b5f7e3
Miguel Alves Cordeiro Braz <dev.macb@gmail.com>
2024-09-10 17:30:47 -0300

    Remove todos os arquivos do projeto

--- src/app/pages/index.ts
+++
@@ -1,6 +0,0 @@
-// Exportação das páginas da aplicação
-export * from './inicio/Inicio';
-export * from './sobre/Sobre';
-export * from './habilidades/Habilidades';
-export * from './portifolio/Portifolio';
-export * from './contato/Contato';
